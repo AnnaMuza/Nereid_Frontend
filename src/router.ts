db77f01943b53b1cfcbd04d0b5cb1af7032d368d
Anna Muzychyna <annamuz69@gmail.com>
2025-03-02 18:48:42 +0200

    template init

---
+++ src/router.ts
@@ -0,0 +1,55 @@
+import AppLayout from '@/layout/AppLayout.vue';
+import { createRouter, createWebHistory } from 'vue-router';
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/',
+            component: AppLayout,
+            meta: {
+                requiresAuth: true
+            },
+            children: [
+                {
+                    path: '/',
+                    name: 'dashboard',
+                    component: () => import('@/views/Dashboard.vue')
+                },
+            ]
+        },
+        {
+            path: '/auth/login',
+            name: 'login',
+            component: () => import('@/views/pages/auth/Login.vue')
+        },
+        {
+            path: '/auth/access',
+            name: 'accessDenied',
+            component: () => import('@/views/pages/auth/Access.vue')
+        },
+        {
+            path: '/auth/error',
+            name: 'error',
+            component: () => import('@/views/pages/auth/Error.vue')
+        }
+    ]
+});
+
+router.beforeEach((to, from, next) => {
+    if (to.meta.requiresAuth) {
+        const token = localStorage.getItem('token');
+        if (token) {
+            // User is authenticated, proceed to the route
+            next();
+        } else {
+            // User is not authenticated, redirect to login
+            next('/auth/login');
+        }
+    } else {
+        // Non-protected route, allow access
+        next();
+    }
+});
+
+export default router;
---
+++ vite.config.mts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+import Components from 'unplugin-vue-components/vite';
+import {PrimeVueResolver} from '@primevue/auto-import-resolver';
+
+export default defineConfig({
+    plugins: [
+        vue(),
+        Components({
+            resolvers: [
+                PrimeVueResolver()
+            ]
+        })],
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, 'src')
+        }
+    }
+})
